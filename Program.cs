1f8702fdb666bb56a62a98850bf4fe8344d2b435
CarvalhoNathan <fariasnathan75@gmail.com>
2025-03-19 23:09:10 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string nome;
+string sobrenome;
+
+Console.Clear();
+
+Console.Write("Digite seu nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
